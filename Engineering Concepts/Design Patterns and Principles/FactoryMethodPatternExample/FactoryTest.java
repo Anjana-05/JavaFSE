5c03cfc1e41c604d4222bdf2a2bfa572924005ac
Anjana Baskaran <anjanabaskaran2005@gmail.com>
2026-06-22 12:00:52 +0530

    Implemented Factory Method Pattern

---
+++ Engineering Concepts/Design Patterns and Principles/FactoryMethodPatternExample/FactoryTest.java
@@ -0,0 +1,12 @@
+package FactoryMethodPatternExample;
+
+public class FactoryTest {
+
+    public static void main(String[] args) {
+
+        DocumentFactory factory = new PdfFactory();
+
+        Document doc = factory.createDocument();
+        doc.open();
+    }
+}
